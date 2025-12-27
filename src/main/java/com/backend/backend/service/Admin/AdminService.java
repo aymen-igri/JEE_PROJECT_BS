c49cd671bb4ea149eee66ecf7d0d03d0a4ac0fb1
Aymen Igri <aymenigri@gmail.com>
2025-12-27 02:05:27 +0100

    Adding the log system functionality to the services and the verification of the doctorApplication status

--- src/main/java/com/backend/backend/service/Admin/AdminService.java
+++ src/main/java/com/backend/backend/service/Admin/AdminService.java
@@ -3,12 +3,16 @@ package com.backend.backend.service.Admin;
 import com.backend.backend.dto.request.Admin.AdminSignupRequest;
 import com.backend.backend.dto.response.Admin.AdminResponse;
 import com.backend.backend.entity.User.Admin;
+import com.backend.backend.entity.activity.ActivityLog;
 import com.backend.backend.mapper.Admin.AdminMapper;
+import com.backend.backend.repository.activity.ActivityLogRepository;
 import com.backend.backend.repository.user.AdminRepository;
 import com.backend.backend.repository.user.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AdminService {
@@ -17,13 +21,20 @@ public class AdminService {
     private final AdminMapper adminMapper;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final ActivityLogRepository activityLogRepository;
 
-
-    public AdminService(AdminRepository adminRepository, AdminMapper adminMapper, PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public AdminService(
+            AdminRepository adminRepository,
+            AdminMapper adminMapper,
+            PasswordEncoder passwordEncoder,
+            UserRepository userRepository,
+            ActivityLogRepository activityLogRepository
+    ) {
         this.adminRepository = adminRepository;
         this.adminMapper = adminMapper;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.activityLogRepository = activityLogRepository;
     }
 
     @Transactional
@@ -46,6 +57,13 @@ public class AdminService {
         admin.setPassword(passwordEncoder.encode(adminRequest.credentials().password()));
 
         Admin savedAdmin = adminRepository.save(admin);
+
+        ActivityLog adminLog = new ActivityLog();
+        adminLog.setUser(adminRepository.findAdminByUserId(savedAdmin.getRegisteredBySuperAdmin().getUserId()));
+        adminLog.setAction("Admin account with ID: " + savedAdmin.getUserId() + " created");
+        adminLog.setEntityType("Admin");
+        adminLog.setTimestamp(LocalDateTime.now());
+        activityLogRepository.save(adminLog);
 
         return adminMapper.toAdminDTO(savedAdmin);
     }
